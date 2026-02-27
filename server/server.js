be54b8223beec6be80a8be7e7e038e638691ae6b
abhi1224 <abhishek27501@gmail.com>
2026-02-27 10:57:01 +0530

    feat: database connection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import { connectDB } from './config/db.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+connectDB()
+
 app.get('/', (req, res) => {
     res.send("Api is working")
 })
+
 
 const PORT = process.env.PORT || 3000
 
