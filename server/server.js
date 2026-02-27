53cab7da8c42f1d4973538852630e999a4906ceb
abhi1224 <abhishek27501@gmail.com>
2026-02-27 10:01:33 +0530

    chore: initialize backend project

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Api is working")
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+    
+})
+
+export default app
