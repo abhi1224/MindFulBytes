affce5f34bdd85432a7702f3b43b7465202e09a2
Abhishek Verma <abhishek27501@gmail.com>
2026-02-28 10:19:04 +0530

    feat: get all blog controller

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, getAllBlogs } from "../controllers/blogController.js";
 import upload from "../middlewares/multer.js";
 import authMiddleware from "../middlewares/auth.js";
 
 const blogRouter = express.Router()
 
 blogRouter.post('/add',authMiddleware, upload.single('upload'), addBlog)
+blogRouter.get('/all', getAllBlogs)
 
 
 export default blogRouter
